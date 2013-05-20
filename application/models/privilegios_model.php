a3b3503a02d976fb023072cb44fdf08216f434e9
beiker <oscar.alcantars@gmail.com>
2013-05-20 18:18:37 -0500

    X

--- application/models/privilegios_model.php
+++ application/models/privilegios_model.php
@@ -7,12 +7,12 @@ class privilegios_model extends CI_Model{
 	 * @var unknown_type
 	 */
 	public $excepcion_privilegio = array();
-	
-	
+
+
 	function __construct(){
 		parent::__construct();
 	}
-	
+
 	/**
 	 * Obtiene el listado de todos los privilegios paginados
 	 */
@@ -25,12 +25,12 @@ class privilegios_model extends CI_Model{
 		);
 		if($params['result_page'] % $params['result_items_per_page'] == 0)
 			$params['result_page'] = ($params['result_page']/$params['result_items_per_page']);
-		
+
 		//Filtros para buscar
 		if($this->input->get('fnombre') != '')
-			$sql = "WHERE ( lower(nombre) LIKE '%".mb_strtolower($this->input->get('fnombre'), 'UTF-8')."%' OR 
+			$sql = "WHERE ( lower(nombre) LIKE '%".mb_strtolower($this->input->get('fnombre'), 'UTF-8')."%' OR
 				lower(url_accion) LIKE '%".mb_strtolower($this->input->get('fnombre'), 'UTF-8')."%' )";
-		
+
 		$query = BDUtil::pagination("
 			SELECT id AS id_privilegio, id_padre, nombre, mostrar_menu, url_accion
 			FROM privilegios
@@ -38,7 +38,7 @@ class privilegios_model extends CI_Model{
 			ORDER BY url_accion ASC
 		", $params, true);
 		$res = $this->db->query($query['query']);
-		
+
 		$response = array(
 				'privilegios' => array(),
 				'total_rows' 		=> $query['total_rows'],
@@ -47,10 +47,10 @@ class privilegios_model extends CI_Model{
 		);
 		if($res->num_rows() > 0)
 			$response['privilegios'] = $res->result();
-		
+
 		return $response;
 	}
-	
+
 	/**
 	 * Obtiene toda la informacion de un privilegio
 	 * @param unknown_type $id
@@ -66,7 +66,7 @@ class privilegios_model extends CI_Model{
 		else
 			return false;
 	}
-	
+
 	/**
 	 * Modifica la informacion de un privilegio
 	 */
@@ -82,7 +82,7 @@ class privilegios_model extends CI_Model{
 		$this->db->update('privilegios', $data, "id = '".$_GET['id']."'");
 		return array(true, '');
 	}
-	
+
 	/**
 	 * Agrega un privilegio a la bd
 	 */
@@ -98,7 +98,7 @@ class privilegios_model extends CI_Model{
 		$this->db->insert('privilegios', $data);
 		return array(true, '');
 	}
-	
+
 	/**
 	 * Elimina un privilegio de la bd
 	 */
@@ -106,15 +106,15 @@ class privilegios_model extends CI_Model{
 		$this->db->delete('privilegios', "id_privilegio = '".$_GET['id']."'");
 		return array(true, '');
 	}
-	
-	
-	
-	
+
+
+
+
 	/**
 		* Verifica si el usuairo tiene ese privilegio, si lo tiene genera un link para accederlo
 		* @param unknown_type $url_accion
 		* @param unknown_type $config = array(
-		*			'params'    => 'id=1&tipo=2', 
+		*			'params'    => 'id=1&tipo=2',
 		*			'btn_type'  => 'btn-danger',
 		*			'icon_type' => 'icon-white',
 		*			'attrs'     => array('onclick' => "msb.confirm('Estas seguro de eliminar el artículo?', 'Publicaciones', this); return false;")
@@ -125,7 +125,7 @@ class privilegios_model extends CI_Model{
 		$priv = $this->tienePrivilegioDe('', $url_accion, true);
 		if(is_object($priv)){
 			$conf = array(
-				'params'    => '', 
+				'params'    => '',
 				'btn_type'  => '',
 				'icon_type' => 'icon-white',
 				'attrs'     => array(),
@@ -143,7 +143,7 @@ class privilegios_model extends CI_Model{
 		}
 		return $txt;
 	}
-	
+
 	/**
 	 * Verifica si el usuario tiene ese privilegio de alerta, si lo tiene genera el html de la alerta con sus datos
 	 * @param unknown_type $url_accion
@@ -157,8 +157,8 @@ class privilegios_model extends CI_Model{
 		}
 		return $txt;
 	}
-	
-	
+
+
 	/**
 	 * Verifica si el usuario tiene un privilegio en espesifico
 	 * @param unknown_type $id_privilegio
@@ -168,9 +168,9 @@ class privilegios_model extends CI_Model{
 	public function tienePrivilegioDe($id_privilegio="", $url_accion="", $returninfo=false){
 		$band = false;
 		$url_accion = str_replace('index/', '', $url_accion);
-		
+
 		$excluir = array_search($url_accion, $this->excepcion_privilegio);
-		
+
 		$sql = $id_privilegio!=''? "p.id = '".$id_privilegio."'": "lower(url_accion) = lower('".$url_accion."')";
 		$res = $this->db
 			->select('p.id, p.nombre, p.url_accion, p.mostrar_menu, p.url_icono')
@@ -188,14 +188,14 @@ class privilegios_model extends CI_Model{
 			return true;
 		return $band;
 	}
-	
+
 	public function getFrmPrivilegios($id_submenu=0, $firs=true, $tipo=null, $showp=false){
 		$txt = "";
 		$bande = true;
-		
+
 		$res = $this->db
-			->select("p.id, p.nombre, p.id_padre, p.url_accion, p.url_icono, p.target_blank, 
-				(SELECT count(privilegio_id) FROM usuarios_privilegios WHERE usuario_id = '".$this->session->userdata('id')."' 
+			->select("p.id, p.nombre, p.id_padre, p.url_accion, p.url_icono, p.target_blank,
+				(SELECT count(privilegio_id) FROM usuarios_privilegios WHERE usuario_id = '".$this->session->userdata('id')."'
 					AND privilegio_id = p.id) as tiene_p")
 			->from('privilegios AS p')
 			->where("p.id_padre = '".$id_submenu."'")
@@ -209,28 +209,28 @@ class privilegios_model extends CI_Model{
 				->where("p.id_padre = '".$data->id."'")
 			->get();
 			$data1 = $res1->row();
-			
+
 			if($tipo != null && !is_array($tipo)){
 				$set_nombre = 'dprivilegios';
 				$set_val = set_radio($set_nombre, $data->id, ($tipo==$data->id? true: false));
 				$tipo_obj = 'radio';
-			}else{	
+			}else{
 				$set_nombre = 'dprivilegios[]';
 				if(is_array($tipo))
-					$set_val = set_checkbox($set_nombre, $data->id, 
+					$set_val = set_checkbox($set_nombre, $data->id,
 							(array_search($data->id, $tipo)!==false? true: false) );
 				else
 					$set_val = set_checkbox($set_nombre, $data->id);
 				$tipo_obj = 'checkbox';
 			}
-			
+
 			if($bande==true && $firs==true && $showp==true){
 				$txt .= '<li><label style="font-size:11px;">
 				<input type="'.$tipo_obj.'" name="'.$set_nombre.'" data-uniform="false" value="0" '.$set_val.($data->id_padre==0?  ' checked': '').'> Padre</label>
 				</li>';
 				$bande = false;
 			}
-			
+
 			if($data1->num > 0){
 				$txt .= '<li><label style="font-size:11px;">
 					<input type="'.$tipo_obj.'" name="'.$set_nombre.'" data-uniform="false" value="'.$data->id.'" '.$set_val.'> '.$data->nombre.'</label>
@@ -245,10 +245,10 @@ class privilegios_model extends CI_Model{
 		}
 		$txt .= '</ul>';
 		$res->free_result();
-		
+
 		return $txt;
 	}
-	
+
 	/**
 	 * Genera el menu izq con los privilegios q el usuario tenga asignados
 	 * @param unknown_type $id_submenu
@@ -257,7 +257,7 @@ class privilegios_model extends CI_Model{
 	public function generaMenuPrivilegio($id_submenu=0, $firs=true){
 		$txt = "";
 		$bande = true;
-		
+
 		$res = $this->db
 			->select('p.id, p.nombre, p.id_padre, p.url_accion, p.url_icono, p.target_blank')
 			->from('privilegios AS p')
@@ -273,10 +273,10 @@ class privilegios_model extends CI_Model{
 				->where("ep.usuario_id = '".$this->session->userdata('id')."' AND p.id_padre = '".$data->id."' AND mostrar_menu = 1")
 			->get();
 			$data1 = $res1->row();
-			
+
 			$link_tar = $data->target_blank==1? ' target="_blank"': '';
-			
-			
+
+
 			if($data1->num > 0){
 				$txt .= '
 				<li'.($firs==false? ' class="submenu parent"': ' class="parent"').'>
@@ -305,7 +305,7 @@ class privilegios_model extends CI_Model{
 					</a>
 				</li>';
 			}
-			
+
 		}
 		return $txt;
 	}
